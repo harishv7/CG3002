d760e272e62b9a9a36fcda836035cc5f61663c4e
Harish V <harishv207@gmail.com>
2016-09-25 00:37:19 +0800

    Ard2Pi.py

---
+++ Ard2Pi.py
@@ -0,0 +1,58 @@
+import serial
+import struct
+import binascii
+
+def readlineCR(port):
+        temp = []
+        data = []
+        ch = port.read()
+        size = 0
+        size = int(binascii.hexlify(ch), 16)
+        ch = port.read()
+        
+        while (ch != b'\r' and ch != b''):
+                # If the len of arr is less than 4,
+                # we add it to the array after converting the received
+                # bytes into hexadecimal
+                if(len(temp) < 4):
+                        temp.append(binascii.hexlify(ch))
+                else:
+                        b = convert_to_float(temp)
+                        data.append(b[0])
+                        temp = []
+                        temp.append(binascii.hexlify(ch))
+                ch = port.read()
+
+        # Append the last reading into the return data
+        try:
+                data.append(convert_to_float(temp)[0])
+        except:
+                print("Exception")
+
+        # Return a tuple containing the size and data
+        return (size, data)
+
+# Convert the array of bytes into float rep
+def convert_to_float(data):
+        data.reverse()
+        int_data = [int(x, 16) for x in data]
+        b = struct.pack('4B', *int_data)
+        b = struct.unpack('>f', b)
+        return b
+
+port = serial.Serial(
+          "/dev/ttyAMA0",
+          baudrate = 115200,
+          timeout = 0
+          )
+
+ctr = 0
+
+while True:
+        if (port.inWaiting() > 0):
+                ctr += 1
+                data = readlineCR(port)
+                print("Iteration " + str(ctr))
+                print(data)
+
+                
