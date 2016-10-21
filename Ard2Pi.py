7d34e989fc430671349eb2dca1a7b718a8b6fbb8
Harish V <harishv207@gmail.com>
2016-10-21 23:42:32 +0800

    Revert "thingy"
    This reverts commit 0697890f5fd942b85aad08f06a56a186d751ea42.

--- Ard2Pi.py
+++ Ard2Pi.py
@@ -78,20 +78,16 @@ def readlineCR(port):
     else:
         current_byte = port.read()
         packet_code = int(binascii.hexlify(current_byte), 16)
+       
+    current_byte = port.read()
+    size = int(binascii.hexlify(current_byte), 16)
     
-    try:
+    stepcount_bytes = []
+    for i in range(0, 4):
         current_byte = port.read()
-        size = int(binascii.hexlify(current_byte), 16)
-    
-        stepcount_bytes = []
-        for i in range(0, 4):
-            current_byte = port.read()
-            stepcount_bytes.append(binascii.hexlify(current_byte))
+        stepcount_bytes.append(binascii.hexlify(current_byte))
         
-            stepcount = convert_to_int(stepcount_bytes)[0]
-    except:
-        port.flushInput()
-        return (float('NaN'), float('NaN'), float('NaN'), float('NaN'), float('NaN'))
+    stepcount = convert_to_int(stepcount_bytes)[0]
 
     computed_checksum = computed_checksum ^ stepcount
     
